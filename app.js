c199f2da06445063aec81ae8c0e9fa00422e5d8f
sultan254 <abubakarali3w@gmail.com>
2020-07-19 19:44:46 +0300

    initial version

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        var txt = $("#txt").val()
+        $.getJSON("https://www.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+        {
+            tags: txt,
+            format: "json"
+
+        },
+        function (data){
+            var photoHtml ="<div class = 'row'>";
+            $.each(data.items,function(i,photo){
+                photoHtml += "<div class= 'col-lg-3 col-md-4 col-6 mt-3'>";
+                photoHtml += '<a href= " ' + photo.link+'  " class= "d-block mb-4 h-100">';
+                photoHtml += '<img src= " ' + photo.media.m + '  " class= "img-fluid h-100" ></div>';
+            });
+            photoHtml += "</div>"
+            $("#photos").html(photoHtml);
+
+        }
+        );
+        
+    })
+});
